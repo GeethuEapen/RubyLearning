f844b9a954040fcf9a8a1a19396ac265d7a1e0c4
geethu <geethu@mobme.(none)>
2012-06-19 17:09:30 +0530

    script to perform insertion to a database

---
+++ ruby_database.rb
@@ -0,0 +1,20 @@
+require 'mysql'
+
+con=Mysql.new('localhost','root','aspirine','test')
+result = con.query('select * from trials')
+
+result.each do |row|
+  puts row[1]
+end
+
+
+data = [{"name" => "Foo", "updatedat" => "foo"}, {"name" => "Bar", "updatedat" => "bar"}]
+
+data.each do |row|
+  con.query("INSERT INTO trials(name,updated_at) VALUES ('#{row["name"]}','#{row["updatedat"]}')")
+end
+
+con.close
+
+
+""
