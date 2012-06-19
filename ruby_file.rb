12260fed937b99c79acf02fcde6ea6374b3a0b33
geethu <geethu@mobme.(none)>
2012-06-19 14:15:04 +0530

    script to write to a file

--- ruby_file.rb
+++ ruby_file.rb
@@ -4,8 +4,16 @@ list = []
 p f1
 arr = f1.readlines
 arr.each do |item|
- list << item
+ list << item.strip
   end
 p list
+
+f2 =File.open("/home/geethu/Downloads/trials2.txt", "w")
+
+list.each do|item|
+  f2.write(item + "\n")
+end
+
+f2.close
 
 
